ed917832e47527d522db46eea40b49e9b8ab95ac
dstuven <dstuven@alumnos.inf.utfsm.cl>
2024-10-19 23:09:44 -0300

    Corrección de errores post merge :)

--- inf23620241/inf236-frontend/src/components/vistaJefeMotores.vue
+++ inf23620241/inf236-frontend/src/components/vistaJefeMotores.vue
@@ -1,5 +1,4 @@
 <template>
-  <div>
   <h2>Vista Jefe Motores</h2>
   <div class="posicionboton">
     <nav>
@@ -12,31 +11,26 @@
     <RouterView />
   </div>
     <div class="containerGeneral">
-      <div class="radio-inputs">
-        <!-- Pestañas superiores-->
+    <div class="radio-inputs">
+    <!-- Pestañas superiores-->
     <label class="radio">
-          <input type="radio" name="radio" value="motor" v-model="selectedOption" />
-          <span class="name">Crear Motor</span>
-        </label>
-        <label class="radio">
-          <input type="radio" name="radio" value="camion" v-model="selectedOption" />
-          <span class="name">Crear Camión</span>
-        </label>
-        <label class="radio">
-          <input type="radio" name="radio" value="asign" v-model="selectedOption" />
-          <span class="name">Manejo Asignaciones Motor</span>
+      <input type="radio" name="radio" value="motor" v-model="selectedOption" />
+      <span class="name">Crear Motor</span>
+    </label>
+    <label class="radio">
+      <input type="radio" name="radio" value="camion" v-model="selectedOption" />
+      <span class="name">Crear Camión</span>
+    </label>
+    <label class="radio">
+      <input type="radio" name="radio" value="asign" v-model="selectedOption" />
+      <span class="name">Manejo Asignaciones Motor</span>
     </label>
 
      <!-- Aqui iria el manejo de las incidencias
     <label class="radio">
       <input type="radio" name="radio" value="asignincidencia" v-model="selectedOption" />
       <span class="name">Manejo Asignaciones Incidencia</span>
-        </label>
-        <label class="radio">
-          <input type="radio" name="radio" value="list" v-model="selectedOption" />
-          <span class="name">Lista de Camiones</span>
-        </label>
-      </div>
+    </label>
     -->
     
     </div>
@@ -278,45 +272,20 @@
   </div>
   -->
 </template>
+
 <script>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import axios from 'axios';
 import { useRouter } from 'vue-router';
 
 export default{
-  name: 'VistaJefeMotores',
+  name: 'App',
   setup() {
     const router = useRouter();
-    const camiones = ref([]);
-    const fetchCamiones = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/api/camiones/');
-        camiones.value = response.data;
-      } catch (error) {
-        console.error('Error fetching camiones:', error);
-      }
-    };
-
-    const updateCamion = async (camion) => {
-      try {
-        const updatedEstado = camion.estado === 'Activo' ? 'Inactivo' : 'Activo';
-        await axios.patch(`http://localhost:8000/api/camiones/${camion.id_camion}/`, {
-          estado: updatedEstado
-        });
-        camion.estado = updatedEstado;
-      } catch (error) {
-        console.error('Error updating camion:', error);
-      }
-    };
-
-    onMounted(() => {
-      fetchCamiones();
-    });
     return {
       selectedOption: ref('motor'),
       router
     };
-    
   },
 
           
@@ -364,7 +333,7 @@ export default{
       },
       message: '',
       isSuccess: false,
-      responseData: null 
+      responseData: null // Añadido para almacenar la respuesta
     };
   },
   methods: {
@@ -377,7 +346,7 @@ export default{
         this.responseData = response.data; // Almacenar la respuesta en responseData
         this.resetForm();
       } catch (error) {
-        this.message = 'Error al enviar el formulario.'  + JSON.stringify(this.form);
+        this.message = 'Error al enviar el formulario.' + JSON.stringify(this.form);
         this.isSuccess = false;
         if (error.response) {
           this.message += ` Detalles del error: ${error.response.data}`;
@@ -884,54 +853,7 @@ button:hover {
   border-color: #4c8bf5;
   box-shadow: 0 0 20px #4c8bf580;
 }
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
 
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  transition: .4s;
-  border-radius: 34px;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  border-radius: 50%;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  transition: .4s;
-}
-
-input:checked + .slider {
-  background-color: #2196F3;
-}
-
-input:checked + .slider:before {
-  transform: translateX(26px);
-}
-
-.round {
-  border-radius: 34px;
-}
 
 
 </style>
